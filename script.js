e7159819c761279a769d30b975bf676e3e9833a9
joelaspe <joe.laspe@gmail.com>
2023-05-10 16:54:06 -0700

    Added a gradient to the original checkerboard image

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function makeSquare(color) {
-    //const container = document.querySelector('#titleContainer');
     const container = document.body;
     container.style.height = "800px";
     container.style.width = "800px";
     container.style.display = 'flex';
     container.style.flexWrap = 'wrap';
     const div = document.createElement('div');
-    div.style.backgroundColor = color;
     //div.style.border = "1px solid black";
     div.style.height = "12.5%";
     div.style.width = "12.5%";
+    div.style.backgroundColor = color;
+    //div.style.opacity = 0.5;
  
     container.appendChild(div);
 }
@@ -23,11 +23,32 @@ function getRandomColor() {
     return rgb;
 }
 
+var isOdd = false;
+  
+for(let i = 0; i < 8; i++) {
+    for(let j = 0; j < 8; j++) {
+        isOdd = !isOdd;
+        if(isOdd) {
+            makeSquare("red");
+        } else {
+            makeSquare("black");
+        }
+    }
+    //making a new row, swap the isOdd variable because we need to 'repeat' a color
+    isOdd = !isOdd;
+}    
+    
+    
+//makeSquare(getRandomColor());
 
-// run the loop 64 times to get an 8x8 grid
-for(let i = 0; i < 64; i++)
-{
-  makeSquare(getRandomColor());
-}
+
+const gradient = document.createElement('div');
+gradient.style.height = "800px";
+gradient.style.width = "800px";
+gradient.style.opacity = 0.5;
+gradient.style.background = "linear-gradient(45deg, rgb(2,0,36), rgb(44,44,74), rgb(0,212,255) 100%";
+gradient.style.position = "relative";
+gradient.style.bottom = "800px";
+document.body.appendChild(gradient);
 
 
